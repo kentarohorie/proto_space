9032db7816e5413313885581b631786e3baf45eb
堀江健太朗 <kentaro.horie.4@gmail.com>
2015-12-02 22:08:42 +0900

    プロフ編集機能追加
    user_controllerの追加
    editファイル編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
-  def create
-
+  def show
   end
+
+  def edit
+  end
+
+  def update
+    current_user.update(update_params)
+    # binding.pry
+    redirect_to '/' #controller: 'top_controller' action: 'index' and return
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:username, :member, :profile, :works, :avatar, :email)
+  end
+
 end
