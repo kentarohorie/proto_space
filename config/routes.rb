97718a2a0fbcbf0d0b67549ed3dd943d2d6de507
堀江健太朗 <kentaro.horie.4@gmail.com>
2015-12-11 00:51:00 +0900

    投稿view＆プロトタイプ詳細画面整備

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :users, only: [:show, :edit, :update] do
-    resources :protos, only: [:new, :create]
+    resources :protos, only: [:new, :create, :show]
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204075334) do
+ActiveRecord::Schema.define(version: 20151209031851) do
 
   create_table "protos", force: :cascade do |t|
     t.datetime "created_at"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151204075334) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "proto_id",   limit: 4
+    t.integer  "status",     limit: 4
   end
 
   add_index "thumbnails", ["proto_id"], name: "index_thumbnails_on_proto_id", using: :btree
