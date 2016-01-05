e1a33296f639084f435148adb98adb8256db925c
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-01-05 11:29:42 +0900

    commentに関する必要ファイル追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  def create
+    Comment.create(create_params)
+    @comments = Comment.where(proto_id: params[:proto_id])
+  end
+
+  private
+  def create_params
+    params.require(:comment).permit(:comment).merge(proto_id: params[:proto_id])
+  end
+end
