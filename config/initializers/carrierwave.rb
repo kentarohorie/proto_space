5804d5ce9efa0518c7eb7c333f8d8771295476ef
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-01-13 19:52:51 +0900

    no message

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAJGVHQZVEEGYGTXEA',
-      :aws_secret_access_key  => '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',
+      :aws_access_key_id      => 'AKIAJU4BS2SMB5X5427AA',
+      :aws_secret_access_key  => 'arM1Mghfb4buMqKuMlU9D4intks2gr/7Eaj7wEe8',
       :region                 => 'ap-northeast-1'
     }
 
