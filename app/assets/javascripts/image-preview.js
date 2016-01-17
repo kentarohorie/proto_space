b1c413a58014b9b0b61b35ecc2dde686747d984e
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-01-17 15:21:02 +0900

    prototypeのedit画面での画像プレビュー機能追加

--- app/assets/javascripts/image-preview.js
+++ app/assets/javascripts/image-preview.js
@@ -2,4 +2,14 @@ $(function() {
   $('#mainImagePreview input:file').uploadThumbs();
 
   $('#subImagePreview input:file').uploadThumbs();
+
+   $('#mainImagePreview input:file').change(function() {
+    $('#mainImagePreview #preImg').remove();
+  });
+
+   $('#subImagePreview input:file').change(function() {
+    $('#preSubImg').remove();
+   });
 });
+
+
