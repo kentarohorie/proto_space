6516b7f5e140940b91fe8679cd31480c251f39bc
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-01-18 21:44:44 +0900

    prototypeの編集画面、新規投稿画面へJS導入
    jsファイル追記
    js導入によるviewファイル修正

--- app/assets/javascripts/image-preview.js
+++ app/assets/javascripts/image-preview.js
@@ -10,6 +10,42 @@ $(function() {
    $('#subImagePreview input:file').change(function() {
     $('#preSubImg').remove();
    });
+
+    $(function() {
+      $('#newViewPlus').click(function() {
+        $(this).remove();
+        $('#invisibleBlock').css('display', 'block')
+        $('#invisibleBlock input:file').uploadThumbs();
+      });
+    });
+
+    $(function() {
+      if ($('#editViewList1')) {
+        $('#editViewList1').css('display', 'none');
+      };
+
+      if ($('#editViewList2')) {
+        $('#editViewList2').css('display', 'none');
+      };
+
+
+      $('.editViewPlus').click(function() {
+        var listID = $(this).parent('li').attr('id');
+
+        $(this).remove();
+        $('.invisibleBlock').css('display', 'block');
+        $('.invisibleBlock input:file').uploadThumbs();
+
+        if (listID == "editViewList0") {
+          $('#editViewList1').css('display', 'block');
+          $('#editViewList1').children('.image-upload').css('display', 'none');
+        } else if (listID == "editViewList1") {
+          $('#editViewList2').css('display', 'block');
+          $('#editViewList2').children('.image-upload').css('display', 'none');
+        };
+      });
+    });
+
 });
 
 
